0822baea6e63d64e8717ee1a5f1872f4f92cdabc
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-02-01 05:13:42 +0000

    Adds client side dependencies with bower

--- app.js
+++ app.js
@@ -15,29 +15,10 @@ app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
 }));
 
 app.use(express.static(path.join(config.dirs.root, "client")));
+app.use(express.static(path.join(config.dirs.root, "bower_components")));
 
 app.get("/", function(req, res){ 
 	res.sendFile(path.join(__dirname, "/client/index.html"));
-});
-
-app.get("/lib/:name", function(req, res){
-	var script = config.lib[req.params.name];
-	if(!_.isUndefined(script)) {
-		//Remove extension from dependency name
-		var name = req.params.name;
-		name = name.substring(0, name.indexOf("."));
-
-		//Check if dependency is mapped somewhere in the bower directory
-		if( typeof config.libMap[name] !== "undefined" ) {
-			name = config.libMap[name];
-		}
-
-		//Send file or exception
-		res.sendFile(path.join(config.dirs.bower, name, script));
-	} else {
-		//No dependency by that name
-		res.status("404").send("Not Found");
-	}
 });
 
 var bootstrap_socket = require("./resources/socket");
